384350454f05a6c8de6e89a9911ddf04efe3eb84
120L020121 <matrix_250@163.com>
2023-03-07 19:27:04 +0800

    second largest.

--- fun.py
+++ fun.py
@@ -1,3 +1,9 @@
+# random integer
 import random
 print(random.randint(0, 10))
-print("hello world.")
+
+# second largest. If you want smallest, max->min
+nums = [5, 12, 54, 87, 55, 69, 23, 17]
+nums.remove(max(nums))
+second_largest = max(nums)
+print(second_largest)
