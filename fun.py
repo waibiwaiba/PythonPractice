649f91f6b80e867b36fb9b5320c4deb950616e07
120L020121 <matrix_250@163.com>
2023-03-08 09:43:14 +0800

    International Women's Day.

--- fun.py
+++ fun.py
@@ -1,18 +1,54 @@
-# random integer
+"""random integer"""
 import random
 print(random.randint(0, 10))
 
-# second largest. If you want smallest, max->min
+
+"""second largest. If you want smallest, max->min"""
 nums = [5, 12, 54, 87, 55, 69, 23, 17]
 nums.remove(max(nums))
 second_largest = max(nums)
 print(second_largest)
 
-# recursive decimal->binary
+
+"""recursive decimal->binary"""
 def dec_to_binary(n):
     if n > 1:
         dec_to_binary(n // 2)
     print(n % 2, end="")
-num = int(input("Your decimal number: "))
-dec_to_binary(num)
-print(" ")
+print("13")
+dec_to_binary(13)
+print("")
+
+"""recursive reverse string print"""
+def recursive_reverse_str_print(str):
+    print(str[-1], end="")
+    if len(str) > 1:
+        recursive_reverse_str_print(str[:-1])
+str1 = "abcde"
+print(str1)
+recursive_reverse_str_print(str1)
+
+
+'''reverse str using stack'''
+def reverse_stack(str):
+    stack = []
+    for letter in str:
+        stack.append(letter)
+    # stack.reverse(): reverse a list.
+    while len(stack) > 0:
+        print(stack.pop(), end="")
+print("")
+reverse_stack("abcde")
+print("")
+
+
+'''reverse using list.reverse()'''
+str1 = "abcde"
+l1 = list(str1)
+print(l1)
+l1.reverse()
+print(l1)
+for letter in l1:
+    print(letter, end="")
+print("")
+
