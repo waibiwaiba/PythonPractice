d87dd5dea61880ce7c0edec5726783ea122ab08f
120L020121 <matrix_250@163.com>
2023-03-08 08:04:28 +0800

    recursive decimal to binary.

--- fun.py
+++ fun.py
@@ -7,3 +7,12 @@ nums = [5, 12, 54, 87, 55, 69, 23, 17]
 nums.remove(max(nums))
 second_largest = max(nums)
 print(second_largest)
+
+# recursive decimal->binary
+def dec_to_binary(n):
+    if n > 1:
+        dec_to_binary(n // 2)
+    print(n % 2, end="")
+num = int(input("Your decimal number: "))
+dec_to_binary(num)
+print(" ")
